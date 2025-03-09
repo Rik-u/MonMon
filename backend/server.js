e96ee40a1dffc716164681985170cfb67bac6952
Riku Voraniphit <voraniphit.k@ku.th>
2025-03-09 21:40:33 +0700

    Add accounts API routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const server = express();
 const port = 3001;
 
-server.post()
+const accountsRoutes = require('./routes/accounts'); 
+
+server.use(bodyParser.json());
+
+server.use('/accounts', accountsRoutes);
 
 server.listen(port, () => {
     console.log(`Backend server running at http://localhost:${port}`);
