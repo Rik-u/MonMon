54760007c09a97869bf7beb507377248656be775
Riku Voraniphit <voraniphit.k@ku.th>
2025-03-10 11:48:07 +0700

    Finishing up

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 console.log('Loaded PORT:', process.env.PORT);
+const cors = require('cors');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,6 +8,12 @@ const server = express();
 const port = process.env.PORT;
 
 const accountsRoutes = require('./src/routes/accountsRoutes'); 
+
+server.use(cors({
+    origin: 'http://localhost:3000', // Only allows requests from frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    allowedHeaders: ['Content-Type'] // Allowed headers
+}));
 
 server.use(bodyParser.json());
 
